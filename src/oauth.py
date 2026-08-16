d8d619d178a8409f4c14384f9314746e2bb8cb62
FScirE <leo.soleke@gmail.com>
2026-08-16 12:46:01 +0200

    Added comments

--- src/oauth.py
+++ src/oauth.py
@@ -3,9 +3,17 @@ import webbrowser
 from dotenv import get_key
 from urllib.parse import urlparse, parse_qs
 
+# This auth flow assumes it is only the local host that does the authorization.
+# Multiple simultaneous oauth calls would not work.
+
 auth_code = None
 
 class Handler(http.server.BaseHTTPRequestHandler):
+    """
+    Request handler for the local http server that
+    stores a received OAuth code and returns whether
+    successful or not.
+    """
     def do_GET(self):
         global auth_code
 
@@ -24,7 +32,13 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
 
-def get_oauth_code():
+def get_oauth_code() -> str:
+    """
+    Creates a local http server that handles one request
+    with an OAuth code. Opens a browser window with bungie login,
+    which redirects to a GitHub Pages site that sends a request
+    to the local http server with the received code.
+    """
     global auth_code
     auth_code = None
 
