c7a9c80f009fccc58c05e8e3c2a0cb38bffcf025
FScirE <leo.soleke@gmail.com>
2026-08-17 11:59:22 +0200

    Added 3 minute buffer to daily/weekly reset loop check

--- robin.py
+++ robin.py
@@ -245,8 +245,8 @@ if __name__ == "__main__":
         exit(-1)
 
     #run setup every 30 mins to check for daily/weekly resets
-    schedule.every().hour.at(":05").do(setup_destiny_data)
-    schedule.every().hour.at(":35").do(setup_destiny_data)
+    schedule.every().hour.at(":03").do(setup_destiny_data)
+    schedule.every().hour.at(":33").do(setup_destiny_data)
     thread = threading.Thread(target=run_scheduler, daemon=True)
     thread.start()
 
